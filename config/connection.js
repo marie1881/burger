01a2f88516b625ca0d5bb92c206cfdc3673a1315
marie <marie.delborrello11@gmail.com>
2020-01-10 22:04:46 +0800

    connecttion.js updated

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+
+// Node Dependency
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    port     :  3306,
+    user     : 'root',
+    password : 'Cabbage385', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
+
+
+// Export the Connection
+module.exports = connection;
