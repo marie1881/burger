749f844ac1375b8d8c4cd78d8251b3b0661a847c
marie <marie.delborrello11@gmail.com>
2020-01-11 22:05:50 +0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,46 @@
-var mysql = require("mysql");
-var connection;
+// var mysql = require("mysql");
+// var connection;
 
-//If deploying to Heroku, make sure you set up the JAWSDB add-on.
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        port: 3306,
-        host: "localhost",
-        user: "root",
-        password: "Cabbage385",
-        database: "burgers_db"
-    });
-}
+// //If deploying to Heroku, make sure you set up the JAWSDB add-on.
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         port: 3306,
+//         host: "localhost",
+//         user: "root",
+//         password: "Cabbage385",
+//         database: "burgers_db"
+//     });
+// }
+
+// // Make connection.
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+
+var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Cabbage385",
+    database: "burgers_db"
+});
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
