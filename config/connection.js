b69a9ea45108a39a37ed01b7ffaaab291755ba35
marie <marie.delborrello11@gmail.com>
2020-01-11 19:19:15 +0800

    fixing

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
 
-// Node Dependency
-var mysql = require('mysql');
-var connection;
+// Requiring mysql package
+var mysql = require("mysql");
 
-// For Heroku Deployment vs. Local MySQL Database
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
-  connection = mysql.createConnection({
+// Setting up our connection information
+var source = {
+  localhost: {
     host     : 'localhost',
     port     :  3306,
     user     : 'root',
     password : 'Cabbage385', // Add your password
     database : 'burgers_db' // Add your database
-  });
-}
+  }
+};
 
 
-// Export the Connection
+// Creating our connection
+var connection = mysql.createConnection(source.localhost);
+
+
+// Connecting to the database.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Exporting our connection
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -5,14 +5,14 @@ var connection = require('./connection.js');
 
 
 
-// Connect to MySQL database
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  };
-  console.log('connected as id ' + connection.threadId);
-});
+// // Connect to MySQL database
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   };
+//   console.log('connected as id ' + connection.threadId);
+// });
 
 
 
