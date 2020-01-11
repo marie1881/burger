a6761f10c0a979d4664f877dba427a1639d19ae5
marie <marie.delborrello11@gmail.com>
2020-01-11 20:28:11 +0800

    fixing

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,5 @@
 // Node Dependency
-var orm = require('../config/orm.js');
+var orm = require('../config/orm');
 
 
 // create the code that will call the ORM functions using burger specific input for the ORM.
