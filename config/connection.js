667f439079e0ee41bad8b6d128902ea4e439ce8e
marie <marie.delborrello11@gmail.com>
2020-01-11 21:16:44 +0800

    connection.js update

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,53 @@
 
-// Requiring mysql package
+// // Requiring mysql package
 
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-// Setting up our connection information
-var source = {
-  localhost: {
+// // Setting up our connection information
+// var source = {
+//   localhost: {
+//     host     : 'localhost',
+//     port     :  3306,
+//     user     : 'root',
+//     password : 'Cabbage385', // Add your password
+//     database : 'burgers_db' // Add your database
+//   }
+// };
+
+
+// // Creating our connection
+// var connection = mysql.createConnection(source.localhost);
+
+
+// // Connecting to the database.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Exporting our connection
+// module.exports = connection;
+
+
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.PORT){
+  connection = mysql.createConnection(process.env.PORT);
+}
+else{
+  connection = mysql.createConnection({
     host     : 'localhost',
-    port     :  3306,
     user     : 'root',
     password : 'Cabbage385', // Add your password
     database : 'burgers_db' // Add your database
-  }
-};
+  });
+}
 
 
-// Creating our connection
-var connection = mysql.createConnection(source.localhost);
-
-
-// Connecting to the database.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Exporting our connection
+// Export the Connection
 module.exports = connection;
